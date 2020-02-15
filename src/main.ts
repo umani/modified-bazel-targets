cbe93f175308d753a52bc0bf7024f92aa6418d62
Juliana Oliveira <juliana@uma.ni>
2020-02-15 20:47:16 -0300

    feat(main): add bazel target processing function
    Signed-off-by: Juliana Oliveira <juliana@uma.ni>

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,41 @@
-import * as core from '@actions/core'
+import * as core from "@actions/core"
+import { dirname, join, parse } from "path"
+import { exec } from "child_process"
+import { fs } from "fs"
+import { promisify } from "util"
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+async function findBuildBazelFile(currentPath: string, basePath: string): Promise<string | null> {
+    try {
+      await fs.promises.access(join(currentPath, "BUILD.bazel"), fs.constants.F_OK)
+      return currentPath
+    } catch {
+      if (currentPath === basePath || basePath.indexOf(currentPath) !== -1) {
+        return null
+      } else {
+        return (await findBuildBazelFile(dirname(currentPath), basePath))
+      }
+    }
+}
 
-    core.debug(new Date().toTimeString())
-    core.debug(new Date().toTimeString())
+export async function findBazelBuilds(changedFiles: string[], basePath: string = '.'): Promise<Set<string>> {
+    return new Set(
+        (await Promise.all(
+          changedFiles.map(file => findBuildBazelFile(file, basePath))
+        ))
+      .filter((f): f is string => f !== null))
+}
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+export const bazelTargets = async (input: Set<string>): Promise<string> => {
+   const { stdout, stderr } = await promisify(exec)(`bazel query 'rdeps(${Array.from(input).join(', ')})'`)
+   return stdout
+}
+
+export async function run(): Promise<void> {
+    const changedFiles: string = core.getInput('changed-files')
+    const bazelBuilds = await findBazelBuilds(changedFiles.split(" "))
+    const processedTargets = await bazelTargets(bazelBuilds)
+    core.debug(`bazel targets: ${processedTargets}`)
+    core.setOutput('bazel-targets', processedTargets)
 }
 
 run()
